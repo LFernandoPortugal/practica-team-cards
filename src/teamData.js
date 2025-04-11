e124606c48e4bbb670695e5d4f2761706c5448b5
falconblade <falconbladex1@gmail.com>
2025-04-11 17:45:51 -0500

    cambio imagenes a public

--- src/teamData.js
+++ src/teamData.js
@@ -3,36 +3,36 @@ export const teamMembers = [
         id: 1,
       name: "Bill Mahoney",
       role: "PRODUCT OWNER",
-      image: "./src/images/photo1.png"
+      image: "images/photo1.png"
     },
     {
         id: 2,
       name: "Saba Cabrera",
       role: "ART DIRECTOR",
-      image: "./src/images/photo2.png"
+      image: "images/photo2.png"
     },
     {
         id: 3,
       name: "Shae Le",
       role: "TECH LEAD",
-      image: "./src/images/photo3.png"
+      image: "images/photo3.png"
     },
     {
         id: 4,
       name: "Skylah Lu",
       role: "UX DESIGNER",
-      image: "./src/images/photo4.png"
+      image: "images/photo4.png"
     },
     {
         id: 5,
       name: "Griff Richards",
       role: "DEVELOPER",
-      image: "./src/images/photo5.png"
+      image: "images/photo5.png"
     },
     {
         id: 6,
       name: "Stan John",
       role: "DEVELOPER",
-      image: "./src/images/photo6.png"
+      image: "images/photo6.png"
     }
   ];
